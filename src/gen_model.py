75ac677e2a5521cd76ce281ae2757f12a74ef82c
Lese <2385342343@qq.com>
2023-12-31 15:43:04 +0800

    perf: add a prompt to the code that generates the model

--- src/gen_model.py
+++ src/gen_model.py
@@ -8,8 +8,12 @@ env = AlphaSeer()
 #设置AI玩家agent
 agent = DQN('MlpPolicy', env, verbose=0,tensorboard_log='logs')
 
+print("start learning")
 #对战场次定为50万次
 agent.learn(total_timesteps=500000, log_interval=100,tb_log_name='DQN')
 
+print("start saving the model")
 #保存模型
 agent.save("model/DQN_Seer")
+
+print("All over")
